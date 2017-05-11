91114d0fc449cc9e0adbbcada3213bb6c400ec4f
Manu Shivakumar <manu.machilles@gmail.com>
2017-05-11 19:34:37 +0530

    Correcting Project Name and hence the solutions

---
+++ Algorithms/Program.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Strings;
+using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace Algorithms
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			DateTime start = DateTime.Now;
+	
+			StringPermutation.Permute("abcde", "");
+			DateTime end = DateTime.Now;
+			
+			Console.WriteLine((end-start).Milliseconds);
+			start = DateTime.Now;
+			StringPermutation.RecursivePermute("abcde".ToCharArray(), 0);
+			end = DateTime.Now;
+			
+			Console.WriteLine((end-start).Milliseconds);
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
